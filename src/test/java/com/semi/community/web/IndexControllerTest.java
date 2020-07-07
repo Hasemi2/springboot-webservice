13abc119a37a8d7f37fe43ce296db115885b9765
hasemi <h171754@gmail.com>
2020-07-07 18:59:04 +0900

    [20200707] 등록, 수정화면 테스트 코드 작성

--- src/test/java/com/semi/community/web/IndexControllerTest.java
+++ src/test/java/com/semi/community/web/IndexControllerTest.java
@@ -1,5 +1,7 @@
 package com.semi.community.web;
 
+import com.semi.community.domain.posts.Posts;
+import com.semi.community.domain.posts.PostsRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +18,27 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private PostsRepository postsRepository;
+
     @Test
     public void mainLoading() {
         String body = this.restTemplate.getForObject("/" , String.class);
+        assertThat(body).contains("등록");
+    }
 
-        assertThat(body).contains("test");
+    @Test
+    public void saveLoading() {
+        String body = this.restTemplate.getForObject("/posts/save" , String.class);
+        assertThat(body).contains("등록");
+    }
+
+    @Test
+    public void updateLoading() {
+        Posts savedPosts = postsRepository.save(Posts.builder().title("제목1").author("글쓴이1").content("내용1").build());
+        Long id = savedPosts.getId();
+        String body = this.restTemplate.getForObject("/posts/update/" + id , String.class);
+        assertThat(body).contains(savedPosts.getTitle());
     }
 
 }
